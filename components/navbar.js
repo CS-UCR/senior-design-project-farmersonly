68aa6c8507f9b2ef450d395dfcd50292a6e4086c
Sebastian Garcia <sgarc133@ucr.edu>
2021-11-03 12:00:22 -0700

    landing page and navbar

---
+++ components/navbar.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'; 
+import {Navbar, Nav} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import styles from '../styles/navbar.module.css'
+
+
+export default function navbar(){
+    return (
+      <Navbar bg="dark" variant="dark"  className={styles.container}>
+
+        <Navbar.Brand href="#home">UCR Water</Navbar.Brand>
+
+        <Nav className="me-auto">
+          <Nav.Link href="#landing">Home</Nav.Link>
+          <Nav.Link href="samz">SAMZ-Desert Tool</Nav.Link>
+          <Nav.Link href="#pricing">About</Nav.Link>
+        </Nav>
+      
+    </Navbar>
+    )
+}
---
+++ pages/landing.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/landing.module.css'
+
+export default function landing(){
+    return (
+      <div className={styles.container}>        
+        <h1 className={styles.title}>UCR Irrigation and Water Management Tool Suite</h1>
+      </div> 
+      )
+}
