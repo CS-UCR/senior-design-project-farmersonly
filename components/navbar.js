34d8f9f99b89b5d0af0539462b022a9997badf45
Sebastian Garcia <sgarc133@ucr.edu>
2021-11-08 12:58:44 -0800

    nav bar loads on every page

--- components/navbar.js
+++ components/navbar.js
@@ -7,14 +7,14 @@ import styles from '../styles/navbar.module.css'
 
 export default function navbar(){
     return (
-      <Navbar bg="dark" variant="dark"  className={styles.container}>
+      <Navbar bg="dark" variant="dark" sticky="top" className={styles.container}>
 
         <Navbar.Brand href="#home">UCR Water</Navbar.Brand>
 
         <Nav className="me-auto">
-          <Nav.Link href="#landing">Home</Nav.Link>
+          <Nav.Link href="landing">Home</Nav.Link>
           <Nav.Link href="samz">SAMZ-Desert Tool</Nav.Link>
-          <Nav.Link href="#pricing">About</Nav.Link>
+          <Nav.Link href="about">About</Nav.Link>
         </Nav>
       
     </Navbar>
