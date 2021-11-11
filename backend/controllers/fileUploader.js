f88fa6a5758e5409745dc2ba94a6716711ab51ab
Robert Navarro <robertn2562@gmail.com>
2021-11-11 13:12:40 -0800

    Added python script to backend and configured it to run on uploaded files

--- backend/controllers/fileUploader.js
+++ backend/controllers/fileUploader.js
@@ -15,6 +15,20 @@ function uploadFile(request, response)
           console.error(err)
           return
         }})*/
+    const spawn = require("child_process").spawn;
+    const pythonScript = spawn('python', ['./process.py', path+".xlsx"]);
+    pythonScript.stdout.on('data', function(data) {
+        console.log("before python script")
+        console.log(data.toString());
+    });
+    pythonScript.stderr.on('data', (data)=>
+    {
+        console.error('stderr: ' + data.toString())
+    })
+    pythonScript.on('close', (code)=>
+    {
+        console.error('exited with code:' + code.toString())
+    })
     response.send("hello world");
 }
 module.exports = {
