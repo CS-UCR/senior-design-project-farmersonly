a1b5196c00d5fd4317307d416e54484f0ac31a15
jc2673528 <juan99castellon@gmail.com>
2021-11-12 11:34:55 -0800

    Minor changes to Navbar to work better on every page

--- components/navbar.js
+++ components/navbar.js
@@ -12,10 +12,9 @@ export default function navbar(){
         <Navbar.Brand href="#home">UCR Water</Navbar.Brand>
 
         <Nav className="me-auto">
-          <Nav.Link href="#landing">Home</Nav.Link>
-          <Nav.Link href="samz">SAMZ-Desert Tool</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <Nav.Link href="about">About</Nav.Link>
+          <Nav.Link href="landing">Home</Nav.Link>
+          <Nav.Link href="/samz">SAMZ-Desert Tool</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
         </Nav>
       
     </Navbar>
--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,21 @@
 
 import React from 'react'
-import Navbar from '../components/navbar'
 import styles from '../styles/about.module.css'
-
+import Navbar from '../components/navbar'
+import Head from 'next/head'
 
 export default function test(){
     return(
+        
         <div className = {styles.container}>
-        <h1 className = {styles.title}>About Us</h1>
+            <Head>
+            <title>UCR Water</title>
+            <link rel="icon" href="/favicon.ico" />
+            <Navbar />
+            <h1 className = {styles.title}>About Us</h1>
+
+        </Head>
+
         </div>
     )
 }
