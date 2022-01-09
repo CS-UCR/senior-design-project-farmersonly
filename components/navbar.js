448a1dcfe1c9b668a721b84fb824c8e79cc86439
daniel-nis <danielnissan211@gmail.com>
2022-01-09 01:04:10 -0800

    login page created

--- components/navbar.js
+++ components/navbar.js
@@ -14,6 +14,7 @@ export default function navbar(){
           <Nav.Link href="landing">Home</Nav.Link>
           <Nav.Link href="samz">SAMZ-Desert Tool</Nav.Link>
           <Nav.Link href="about">About</Nav.Link>
+          <Nav.Link href="firebase/signup">Sign Up</Nav.Link>
         </Nav>
       
     </Navbar>
