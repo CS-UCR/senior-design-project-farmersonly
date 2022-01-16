2c6009afee905e9b05694c65872d7967b158ea69
Sebastian Garcia <sgarc133@ucr.edu>
2022-01-16 12:43:16 -0800

    Swicthed Sign Up to Sign In, moved to right side

--- components/navbar.js
+++ components/navbar.js
@@ -1,22 +1,23 @@
-import React, {Component} from 'react'; 
-import {Navbar, Nav} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import styles from '../styles/navbar.module.css'
+import styles from "../styles/navbar.module.css";
 
-export default function navbar(){
-    return (
-      <Navbar bg="dark" variant="dark" sticky="top" className={styles.container}>
+export default function navbar() {
+  return (
+    <Navbar bg="dark" variant="dark" sticky="top" className={styles.container}>
+      <Navbar.Brand>UCR Water</Navbar.Brand>
 
-        <Navbar.Brand>UCR Water</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="landing">Home</Nav.Link>
+        <Nav.Link href="samz">SAMZ-Desert Tool</Nav.Link>
+        <Nav.Link href="about">About</Nav.Link>
+      </Nav>
 
-        <Nav className="me-auto">
-          <Nav.Link href="landing">Home</Nav.Link>
-          <Nav.Link href="samz">SAMZ-Desert Tool</Nav.Link>
-          <Nav.Link href="about">About</Nav.Link>
-          <Nav.Link href="signup">Sign Up</Nav.Link>
-        </Nav>
-      
+      <Nav className="justify-content-end">
+        <Nav.Link href="signin">Sign In</Nav.Link>
+      </Nav>
     </Navbar>
-    )
+  );
 }
