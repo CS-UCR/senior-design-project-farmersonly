c1de23cf99c59927d1d42fe80b5674a3d5ee94ed
Sebastian Garcia <sgarc133@ucr.edu>
2022-01-18 15:19:25 -0800

    Re did about page, contents and styling

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,193 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import Link from "@mui/material/Link";
 
-import React from 'react'
-import styles from '../styles/about.module.css'
-import Navbar from '../components/navbar'
-import Head from 'next/head'
+import { styled } from "@mui/material/styles";
+import styles from "../styles/about.module.css";
+import { CenterFocusStrong } from "@mui/icons-material";
 
-export default function about(){
-    return(
-       <div className={styles.container}>
-        <h1 className={styles.title}>About Us</h1>
-        <h2 className={styles.body}>We are UCR Water, a team
-        partnered with Dr.Akansha from the Hanghverdi Water Management Group.</h2>
-        <h2 className={styles.body}>Our goal is to provide web-based tools 
-        for farmers to better manage their water usage and improve their productivity
-        to tackle California's water scarcity problems.</h2>
-       </div>
-    )
+export default function about() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.about}>
+        <h1 className={styles.aboutTitle}>About</h1>
+
+        <div className={styles.aboutText}>
+          <div>
+            In collaboration with Dr. Akansha Garg, from The Hanghverdi Water
+            Management Group, the Irrigation Water Management Tool Suite(IWO
+            Tool Suit) is designed to help farmers make better decisions about
+            their farms and harvest. These tools main goals are to tackle
+            California's water scarcity problems and increase farming
+            productivity.
+          </div>
+          <br />
+          <div>
+            The Samz Dessert Tool is works on agricultural management zones
+            based on NDVI satellite data in order to devleop optimize
+            agricultural management zones and crop production. After uploading
+            NDVI data, processing will compute the mean, min, max, range, and
+            std of data, performace graph, zone delineation, and field
+            recommendation.
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.team}>
+        <h1 className={styles.teamTitle}>The Team</h1>
+
+        <div>
+          <h2 className={styles.cardsTitle}>Team Advisors</h2>
+
+          <Grid
+            container
+            justifyContent="center"
+            columnSpacing={2}
+            rowSpacing={3}
+          >
+            <Grid item>
+              <Card
+                variant="outlined"
+                sx={{ minWidth: 275 }}
+                style={{
+                  backgroundColor: "#0F4C75",
+                  color: "#BBE1FA",
+                  textAlign: "center",
+                }}
+              >
+                <CardContent>
+                  <Typography>Mariam Salloum</Typography>
+                  <Typography sx={{ fontSize: 13 }}>
+                    Assistant Teaching Professor
+                  </Typography>
+                  <Link href="https://www.linkedin.com/in/mariamsalloum/">
+                    <LinkedInIcon />
+                  </Link>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item>
+              <Card
+                variant="outlined"
+                sx={{ minWidth: 275 }}
+                style={{
+                  backgroundColor: "#0F4C75",
+                  color: "#BBE1FA",
+                  textAlign: "center",
+                }}
+              >
+                <CardContent>
+                  <Typography>Akansha Garg</Typography>
+                  <Typography sx={{ fontSize: 13 }}>
+                    Postdoctoral Research Associate
+                  </Typography>
+                  <Link href="https://www.linkedin.com/in/akanksha-garg-ph-d-2025b417/">
+                    <LinkedInIcon />
+                  </Link>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </div>
+
+        {
+          <div>
+            <h2 className={styles.cardsTitle}>Development Team</h2>
+
+            <Grid
+              container
+              justifyContent="center"
+              columnSpacing={2}
+              rowSpacing={3}
+            >
+              <Grid item>
+                <Card
+                  variant="outlined"
+                  sx={{ minWidth: 275 }}
+                  style={{
+                    backgroundColor: "#0F4C75",
+                    color: "#BBE1FA",
+                    textAlign: "center",
+                  }}
+                >
+                  <CardContent>
+                    <Typography>Sebastian Garcia</Typography>
+                    <Typography sx={{ fontSize: 13 }}>Undergraute</Typography>
+                    <Link href="https://www.linkedin.com/in/sebastian-garcia-ca/">
+                      <LinkedInIcon />
+                    </Link>
+                  </CardContent>
+                </Card>
+              </Grid>
+
+              <Grid item>
+                <Card
+                  variant="outlined"
+                  sx={{ minWidth: 275 }}
+                  style={{
+                    backgroundColor: "#0F4C75",
+                    color: "#BBE1FA",
+                    textAlign: "center",
+                  }}
+                >
+                  <CardContent>
+                    <Typography>Robert Navarro</Typography>
+                    <Typography sx={{ fontSize: 13 }}>Undergraute</Typography>
+                    <Link href="https://www.linkedin.com/in/robert-navarro25/">
+                      <LinkedInIcon />
+                    </Link>
+                  </CardContent>
+                </Card>
+              </Grid>
+
+              <Grid item>
+                <Card
+                  variant="outlined"
+                  sx={{ minWidth: 275 }}
+                  style={{
+                    backgroundColor: "#0F4C75",
+                    color: "#BBE1FA",
+                    textAlign: "center",
+                  }}
+                >
+                  <CardContent>
+                    <Typography>Daniel Nissan</Typography>
+                    <Typography sx={{ fontSize: 13 }}>Undergraute</Typography>
+                    <Link href="https://www.linkedin.com/in/danielnis/">
+                      <LinkedInIcon />
+                    </Link>
+                  </CardContent>
+                </Card>
+              </Grid>
+
+              <Grid item>
+                <Card
+                  variant="outlined"
+                  sx={{ minWidth: 275 }}
+                  style={{
+                    backgroundColor: "#0F4C75",
+                    color: "#BBE1FA",
+                    textAlign: "center",
+                  }}
+                >
+                  <CardContent>
+                    <Typography>Juan Castellon</Typography>
+                    <Typography sx={{ fontSize: 13 }}>Undergraute</Typography>
+                    <Link href="google.com">
+                      <LinkedInIcon />
+                    </Link>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </div>
+        }
+      </div>
+    </div>
+  );
 }
