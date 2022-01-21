141e7fe709ec28d3b45fec1b9786746172f6e914
Juan Castellon <juan99castellon@gmail.com>
2022-01-21 11:06:32 -0800

    Firestore Initialized

--- pages/googleSignInButton.js
+++ pages/googleSignInButton.js
@@ -12,6 +12,8 @@ function googleSignIn(){
     return (
         <div className="googleButton">
             <button onClick={signInWithGoogle}> Sign In With Google </button>
+            <button onClick={location.href='myDocs'} ></button>
+            console.log('Signed in');
             <h1>{name}</h1>
             <h1>{email}</h1>
         </div>
