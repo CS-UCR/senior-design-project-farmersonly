dc14034e890bfa6d713cff650a2274c2dfc76ed5
Sebastian Garcia <sgarc133@ucr.edu>
2022-01-27 23:13:19 -0800

    Spacing between delineation and performance graph

--- pages/samz.js
+++ pages/samz.js
@@ -38,8 +38,8 @@ export class samz extends Component {
       std: 0,
       clusters: 0,
       message: "Waiting on file upload",
-      delineationImage: "0",
-      performanceGraphImage: "0",
+      delineationImage: optimaling,
+      performanceGraphImage: optimaling,
     };
   }
 
@@ -47,7 +47,7 @@ export class samz extends Component {
     //console.log(files[0]);
     let data = new FormData();
     data.append("file", files[0]);
-    
+
     axios.post("http://localhost:5000/samz/post", data, config).then((res) => {
       this.setState({
         mean: res.data.mean,
@@ -118,72 +118,79 @@ export class samz extends Component {
                   margin="dense"
                 />
               </div>
+            </div>
+          </div>
 
-              <div>
-                <List>
-                  <Divider style={{ background: "#BBE1FA" }} />
-                  <ListItem>
-                    <ListItemText
-                      primaryTypographyProps={{ style: ListText }}
-                      primary={"Mean: " + this.state.mean}
-                    />
-                  </ListItem>
-                  <Divider style={{ background: "#BBE1FA" }} />
-                  <ListItem>
-                    <ListItemText
-                      primaryTypographyProps={{ style: ListText }}
-                      primary={"Min: " + this.state.min}
-                    />
-                  </ListItem>
-                  <Divider style={{ background: "#BBE1FA" }} />
-                  <ListItem>
-                    <ListItemText
-                      primaryTypographyProps={{ style: ListText }}
-                      primary={"Max: " + this.state.max}
-                    />
-                  </ListItem>
-                  <Divider style={{ background: "#BBE1FA" }} />
-                  <ListItem>
-                    <ListItemText
-                      primaryTypographyProps={{ style: ListText }}
-                      primary={"STD: " + this.state.std}
-                    />
-                  </ListItem>
-                  <Divider style={{ background: "#BBE1FA" }} />
-                  <ListItem>
-                    <ListItemText
-                      primaryTypographyProps={{ style: ListText }}
-                      primary={"Clusters: " + this.state.clusters}
-                    />
-                  </ListItem>
-                  <Divider style={{ background: "#BBE1FA" }} />
-                  <ListItem>
-                    <ListItemText
-                      primaryTypographyProps={{ style: ListText }}
-                      primary={"Message: " + this.state.message}
-                    />
-                  </ListItem>
-                  <Divider style={{ background: "#BBE1FA" }} />
-                  <div className={styles.piechart}>NDVI Range and Mean</div>
-                  <div>
-                    <Donut pieData={this.state} />
-                  </div>
-                  <Divider style={{ background: "#BBE1FA" }} />
-                </List>
-              </div>
+          <div class="row">
+            <div class="col-4">
+              <List>
+                <Divider style={{ background: "#BBE1FA" }} />
+                <ListItem>
+                  <ListItemText
+                    primaryTypographyProps={{ style: ListText }}
+                    primary={"Mean: " + this.state.mean}
+                  />
+                </ListItem>
+                <Divider style={{ background: "#BBE1FA" }} />
+                <ListItem>
+                  <ListItemText
+                    primaryTypographyProps={{ style: ListText }}
+                    primary={"Min: " + this.state.min}
+                  />
+                </ListItem>
+                <Divider style={{ background: "#BBE1FA" }} />
+                <ListItem>
+                  <ListItemText
+                    primaryTypographyProps={{ style: ListText }}
+                    primary={"Max: " + this.state.max}
+                  />
+                </ListItem>
+                <Divider style={{ background: "#BBE1FA" }} />
+                <ListItem>
+                  <ListItemText
+                    primaryTypographyProps={{ style: ListText }}
+                    primary={"STD: " + this.state.std}
+                  />
+                </ListItem>
+                <Divider style={{ background: "#BBE1FA" }} />
+                <ListItem>
+                  <ListItemText
+                    primaryTypographyProps={{ style: ListText }}
+                    primary={"Clusters: " + this.state.clusters}
+                  />
+                </ListItem>
+                <Divider style={{ background: "#BBE1FA" }} />
+                <ListItem>
+                  <ListItemText
+                    primaryTypographyProps={{ style: ListText }}
+                    primary={"Input variation: " + this.state.message}
+                  />
+                </ListItem>
+                <Divider style={{ background: "#BBE1FA" }} />
+                <ListItem>
+                  <ListItemText
+                    primaryTypographyProps={{ style: ListText }}
+                    primary={"Message: " + this.state.message}
+                  />
+                </ListItem>
+                <Divider style={{ background: "#BBE1FA" }} />
+                <div className={styles.piechart}>NDVI Range and Mean</div>
+                <div>
+                  <Donut pieData={this.state} />
+                </div>
+                <Divider style={{ background: "#BBE1FA" }} />
+              </List>
             </div>
 
             <div class="col-3">
               <img
                 src={`data:image/jpeg;base64,${this.state.delineationImage}`}
-                alt=""
               />
             </div>
-
-            <div class="col-5">
+            <div class="col-5" >
               <img
                 src={`data:image/jpeg;base64,${this.state.performanceGraphImage}`}
-                alt=""
+                className={styles.performanceImg}
               />
             </div>
           </div>
