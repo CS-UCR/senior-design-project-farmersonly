ddecea328b9c8720be3b81bb85d94ac57847a000
Juan Castellon <juan99castellon@gmail.com>
2022-02-15 01:18:25 -0800

    File download completed, wrong conversion

--- pages/userFiles.js
+++ pages/userFiles.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { db, auth } from "../Firebase";
 import { doc, collection, getDocs, query } from "firebase/firestore";
-import { BookSharp } from "@mui/icons-material";
+import XLSX from "xlsx";
 
 export default function userFiles() {
     const userDocs = () => { 
@@ -16,11 +16,17 @@ export default function userFiles() {
                     excelFiles.push({...doc.data(), id: doc.id })
                 });
                 for(var i = 0; i < excelFiles.length; i++){
-                    console.log("File ",i,": \n");
-                    console.log("Name: ",excelFiles[i].name);
-                    console.log("Base64 String: ",excelFiles[i].excel);
-                    console.log("Time Stamp: ",excelFiles[i].timestamp);
-                    console.log("ID: ",excelFiles[i].id);
+                    //console.log("File ",i,": \n");
+                    //console.log("Name: ",excelFiles[i].name);
+                    //console.log("Base64 String: ",excelFiles[i].excel);
+                    //console.log("Time Stamp: ",excelFiles[i].timestamp);
+                    //console.log("ID: ",excelFiles[i].id);
+                    var temp = excelFiles[i].excel;
+                    var workbook = XLSX.read(
+                        temp, {type: 'base64', WTF: false}
+                    );
+                    var csv = XLSX.utils.sheet_to_csv(workbook.Sheets[0]);
+                    XLSX.writeFile(workbook, excelFiles[i].name);
                 }
             })
             .catch(err => {
